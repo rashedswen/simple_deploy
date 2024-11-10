6f66404fa58e819b7d3600212580fd70d33e8441
Rashed <rashed.o@labayh.net>
2024-11-10 14:33:24 +0300

    Refactor deployIos function to accept optional arguments

--- bin/simple_deploy.dart
+++ bin/simple_deploy.dart
@@ -24,7 +24,7 @@ void main(List<String> arguments) async {
     String target = arguments[0].toLowerCase();
     if (target == 'ios') {
       if (Platform.isMacOS) {
-        await deployIos();
+        await deployIos(arguments: arguments);
       } else {
         print('Error: You can only deploy to iOS from MacOS.');
         return;
@@ -37,7 +37,7 @@ void main(List<String> arguments) async {
   }
 }
 
-Future<void> promptAndDeploy() async {
+Future<void> promptAndDeploy({List<String>? arguments}) async {
   if (Platform.isMacOS) {
     print('Choose deployment target:');
     print('1. Android');
@@ -53,9 +53,9 @@ Future<void> promptAndDeploy() async {
   if (choice == '1') {
     await deployAndroid();
   } else if (choice == '2') {
-    await deployIos();
+    await deployIos(arguments: arguments);
   } else if (choice == 'a') {
-    await deployAll();
+    await deployAll(arguments: arguments);
   } else if (choice == 'q') {
     print('Quitting deployment.');
   } else {
@@ -65,27 +65,37 @@ Future<void> promptAndDeploy() async {
   }
 }
 
-Future<void> deployIos() async {
+Future<void> deployIos({List<String>? arguments}) async {
   handleVersionStrategy();
 
   print('Deploying to iOS...');
-  await ios.deploy();
+  try {
+    await ios.deploy(arguments: arguments);
+  } catch (e) {
+    print('Error: $e');
+    await handleVersionStrategyInFailure();
+  }
 }
 
 Future<void> deployAndroid() async {
   handleVersionStrategy();
 
   print('Deploying to Android...');
-  await android.deploy();
+  try {
+    await android.deploy();
+  } catch (e) {
+    print('Error: $e');
+    await handleVersionStrategyInFailure();
+  }
 }
 
-Future<void> deployAll() async {
+Future<void> deployAll({List<String>? arguments}) async {
   handleVersionStrategy();
 
   print('Deploying to all platforms...');
   await deployAndroid();
   if (Platform.isMacOS) {
-    await deployIos();
+    await deployIos(arguments: arguments);
   } else {
     print('iOS deployment is only available on MacOS.');
   }
@@ -102,13 +112,13 @@ Future<void> handleVersionStrategy() async {
   }
   print('versionStrategy: $versionStrategy');
 
-
   if (versionStrategy == 'pubspecIncrement') {
     await incrementBuildNumber();
   } else if (versionStrategy == 'none') {
     // Do nothing
   } else {
-    print('Invalid versionStrategy. Valid values are `none` and `pubspecIncrement`.');
+    print(
+        'Invalid versionStrategy. Valid values are `none` and `pubspecIncrement`.');
     exit(1);
   }
 }
@@ -123,9 +133,53 @@ Future<void> incrementBuildNumber() async {
   final currentVersion = doc['version'] as String;
   final versionParts = currentVersion.split('+');
   final versionNumber = versionParts[0];
-  final currentBuildNumber = int.parse(versionParts.length > 1 ? versionParts[1] : '0');
+  final currentBuildNumber =
+      int.parse(versionParts.length > 1 ? versionParts[1] : '0');
 
   final newBuildNumber = currentBuildNumber + 1;
+  final newVersion = '$versionNumber+$newBuildNumber';
+
+  editor.update(['version'], newVersion);
+  await pubspecFile.writeAsString(editor.toString());
+  print('Updated build number to $newBuildNumber in pubspec.yaml');
+}
+
+Future<void> handleVersionStrategyInFailure() async {
+  final workingDirectory = Directory.current.path;
+  String versionStrategy = 'none';
+  try {
+    final config = await loadConfig(workingDirectory, 'common');
+    versionStrategy = config?['versionStrategy'] ?? 'none';
+  } catch (e) {
+    //
+  }
+  print('versionStrategy: $versionStrategy');
+
+  if (versionStrategy == 'pubspecIncrement') {
+    await decreaseBuildNumber();
+  } else if (versionStrategy == 'none') {
+    // Do nothing
+  } else {
+    print(
+        'Invalid versionStrategy. Valid values are `none` and `pubspecIncrement`.');
+    exit(1);
+  }
+}
+
+Future<void> decreaseBuildNumber() async {
+  final pubspecFile = File('pubspec.yaml');
+  final pubspecContent = await pubspecFile.readAsString();
+
+  final doc = loadYaml(pubspecContent);
+  final editor = YamlEditor(pubspecContent);
+
+  final currentVersion = doc['version'] as String;
+  final versionParts = currentVersion.split('+');
+  final versionNumber = versionParts[0];
+  final currentBuildNumber =
+      int.parse(versionParts.length > 1 ? versionParts[1] : '0');
+
+  final newBuildNumber = currentBuildNumber - 1;
   final newVersion = '$versionNumber+$newBuildNumber';
 
   editor.update(['version'], newVersion);
--- lib/src/deploy_ios.dart
+++ lib/src/deploy_ios.dart
@@ -8,22 +8,18 @@ void handleError(String message) {
   exit(1);
 }
 
-
-
-
-
-Future<void> deploy() async {
+Future<void> deploy({List<String>? arguments}) async {
   final workingDirectory = Directory.current.path;
   final config = await loadConfig(workingDirectory, 'ios');
 
   // Run iOS deployment
   final apiKey = config?['teamKeyId'];
-  if (apiKey==null){
+  if (apiKey == null) {
     print('No teamKeyId supplied');
     exit(1);
   }
   final apiIssuer = config?['developerId'];
-  if (apiIssuer==null){
+  if (apiIssuer == null) {
     print('No developerId supplied');
     exit(1);
   }
@@ -31,24 +27,41 @@ Future<void> deploy() async {
   DateTime startTime = DateTime.now();
 
   bool success = await flutterClean(workingDirectory);
-  if (!success){
+  if (!success) {
     return;
   }
 
+  if (arguments != null && arguments.isNotEmpty) {
+    // arguments = flutter build ios --release --flavor production
+  }
+
   print('Build the iOS .ipa');
-  var result = await Process.run('flutter', ['build', 'ipa'], workingDirectory: workingDirectory, runInShell: true);
+  var result = await Process.run(
+      'flutter', ['build', 'ipa', '--flavor', 'production'],
+      workingDirectory: workingDirectory, runInShell: true);
   if (result.exitCode != 0) {
     handleError('flutter build ipa failed: ${result.stderr}');
   }
   print('Built .ipa file');
 
-
   print('Uploading the IPA to TestFlight');
   // Replace with the actual command for uploading to TestFlight, e.g., using Fastlane or another tool
   result = await Process.run(
-      'xcrun', ['altool', '--upload-app', '--type', 'ios', '--file', '$workingDirectory/build/ios/ipa/app.ipa', '--apiKey', apiKey, '--apiIssuer', apiIssuer],
-      workingDirectory: workingDirectory, runInShell: true
-  );
+      'xcrun',
+      [
+        'altool',
+        '--upload-app',
+        '--type',
+        'ios',
+        '--file',
+        '$workingDirectory/build/ios/ipa/app.ipa',
+        '--apiKey',
+        apiKey,
+        '--apiIssuer',
+        apiIssuer
+      ],
+      workingDirectory: workingDirectory,
+      runInShell: true);
   if (result.exitCode != 0) {
     handleError('Upload to TestFlight failed: ${result.stderr}');
   }
